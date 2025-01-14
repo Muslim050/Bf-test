9cb10b95437abc91e4988005057708526d6cecac
Muslim.Yakhyayev <05-hitman@mail.ru>
2025-01-14 17:54:13 +0500

    init master v0.3

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,9 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react(), svgr(), visualizer({
-    open: true, // Автоматически открывает график в браузере после сборки
-  }),],
+  plugins: [react(), svgr(),],
   build: {
     target: 'esnext', // или 'modules' для более широкой совместимости
     minify: 'esbuild', // или 'terser' для более агрессивного сжатия
